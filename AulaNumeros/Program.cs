1049f06289b4f71d980f4e81f62cb05c999317b3
Kemilly <kemillymotta31@gmail.com>
2025-09-15 10:31:32 -0300

    aula 1509

---
+++ AulaNumeros/Program.cs
@@ -0,0 +1,75 @@
+﻿/* var a = 10; // int
+var b = 10.0; // double
+var c = 10.0m; // decimal
+
+Console.WriteLine($"{a.GetType()} {b.GetType()} {c.GetType()}");
+
+// conversão, ! = sem valor nulo
+Console.WriteLine("Inteiro: ");
+var entrada = Console.ReadLine()!;
+
+int numeroEntrada = Convert.ToInt32(entrada); // admite um possivel valor nulo
+int numeroEntradaDois = int.Parse(entrada); // certeza que tem um valor 
+bool foiConvertido = int.TryParse(entrada, out int numeroEntradaTres);
+
+Console.WriteLine($"{numeroEntrada}, {numeroEntradaDois}, {numeroEntradaTres}");
+
+// conversao explicita (decimal)
+Console.WriteLine("Decimal: ");
+var novaEntrada = Console.ReadLine();
+decimal numeroRealEntrada =
+    Convert.ToDecimal(novaEntrada);
+Console.WriteLine($"{numeroRealEntrada}");  
+
+int numeroRealEntradaInteiro = (int) numeroRealEntrada; */
+
+/*
+Console.Write("X: ");
+double x = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Y: ");
+double y = Convert.ToDouble(Console.ReadLine()!);
+
+
+var soma = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y;
+var resto = x % y;
+
+Console.WriteLine($"Soma: {soma}");
+Console.WriteLine($"Subtração: {subtracao}");
+Console.WriteLine($"Multiplicação: {multiplicacao}");
+Console.WriteLine($"Divisão: {divisao}");
+Console.WriteLine($"Resto: {resto}"); */
+
+/* double n1 = 2, n2 = 3, n3 = 1;
+double r1 = n1 * n2 + n3;
+double r2 = n1 * (n2 + n3);
+double r3 = (n1 * n2) + n3;
+double r4 = ((n1 * n2) + n3) / n1;
+
+Console.WriteLine($"{n1} * {n2} + {n3} = {r1}");
+Console.WriteLine($"{n1} * ({n2} + {n3}) = {r2}");
+Console.WriteLine($"({n1} * {n2}) + {n3} = {r3}");
+Console.WriteLine($"(({n1} * {n2}) + {n3}) / {n1} = {r4}"); */
+
+Console.Write("X: ");
+double x = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Y: ");
+double y = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Z: ");
+double z = Convert.ToDouble(Console.ReadLine()!); 
+
+double media1 = (x + y + z) / 3;
+Console.WriteLine($"A média aritmética entre os números é: {media1:N1}");
+
+Console.Write("A: ");
+decimal a = Convert.ToDecimal(Console.ReadLine()!);
+Console.Write("B: ");
+decimal b = Convert.ToDecimal(Console.ReadLine()!);
+Console.Write("C: ");
+decimal c = Convert.ToDecimal(Console.ReadLine()!);
+
+decimal media2 = (a + b + c) / 3; 
+Console.WriteLine($"A média aritmética entre os números é: {media2:N1}");
+
---
+++ AulaVariaveis/Program.cs
@@ -0,0 +1,34 @@
+﻿//declaração de variáveis 
+string nome;
+char primeiraLetraDoMeuNome;
+int idade;
+double altura;
+decimal salario;
+
+//atribuição de variáveis
+Console.Write("Qual o seu nome? ");
+nome = Console.ReadLine()!.ToUpper();
+Console.Write("Qual a primeira letra do seu nome? ");
+primeiraLetraDoMeuNome = Console.ReadLine()![0];
+Console.WriteLine();
+Console.Write("Qual a sua idade? ");
+idade = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Qual a sua altura? ");
+altura = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Qual o seu salário? ");
+salario = Convert.ToDecimal(Console.ReadLine()!);
+
+Console.WriteLine();
+//com interpolação
+Console.WriteLine($"Meu nome é: {nome}");
+Console.WriteLine($"Primeira Letra do meu nome: {primeiraLetraDoMeuNome}");
+Console.WriteLine($"Eu tenho {idade} anos");
+Console.WriteLine($"Altura: {altura:N2}cm");
+Console.WriteLine($"Salário mensal: {salario:C2}");
+
+/* sem interpolação
+Console.Write("Meu nome é: ");
+Console.WriteLine(nome);
+Console.Write("Primeira Letra do meu nome: ");
+Console.WriteLine(primeiraLetraDoMeuNome);
+*/
